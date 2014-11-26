23d3c045fc3b53c267706b7b997da31f68bb8b37
110chang <u@110chang.com>
2014-11-26 14:49:28 +0900

    [clean] convert indentation, and so
    convert indentation to 2 spaces.
    remove part of vender prefixes from RAF object.

--- source/js/mod/compare.js
+++ source/js/mod/compare.js
@@ -7,33 +7,33 @@
 */
 
 define([], function() {
-	var compare = function(target, original, allowInherit) {
-		if (!target) {
-			throw new Error('Invalid arguments.');
-		}
-		if (!original) {
-			throw new Error('Invalid arguments.');
-		}
-		if (allowInherit === undefined) {
-			allowInherit = true;
-		}
-		var o;
-		
-		for (o in target) {
-			if (original[o] === undefined) {
-				return false;
-			}
-		}
-		// exact match
-		if (!allowInherit) {
-			for (o in original) {
-				if (target[o] === undefined) {
-					return false;
-				}
-			}
-		}
-		return true;
-	};
-	
-	return compare;
+  var compare = function(target, original, allowInherit) {
+    if (!target) {
+      throw new Error('Invalid arguments.');
+    }
+    if (!original) {
+      throw new Error('Invalid arguments.');
+    }
+    if (allowInherit === undefined) {
+      allowInherit = true;
+    }
+    var o;
+    
+    for (o in target) {
+      if (original[o] === undefined) {
+        return false;
+      }
+    }
+    // exact match
+    if (!allowInherit) {
+      for (o in original) {
+        if (target[o] === undefined) {
+          return false;
+        }
+      }
+    }
+    return true;
+  };
+  
+  return compare;
 });
--- source/js/mod/inherit.js
+++ source/js/mod/inherit.js
@@ -7,20 +7,20 @@
 */
 
 define([], function() {
-	var inherit = function() {
-		var i = 0, o = {}, F = function() {}, child, prop;
-		
-		for (; i < arguments.length; i++) {
-			//console.log(arguments);
-			for (prop in arguments[i]) {
-				o[prop] = arguments[i][prop];
-			}
-		}
-		F.prototype = o;
-		child = new F();
-		
-		return child;
-	};
-	
-	return inherit;
+  var inherit = function() {
+    var i = 0, o = {}, F = function() {}, child, prop;
+    
+    for (; i < arguments.length; i++) {
+      //console.log(arguments);
+      for (prop in arguments[i]) {
+        o[prop] = arguments[i][prop];
+      }
+    }
+    F.prototype = o;
+    child = new F();
+    
+    return child;
+  };
+  
+  return inherit;
 });
